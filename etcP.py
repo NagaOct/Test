997666d81a87a7f73a888765aed0d1a9ea55dfef
NagaOct <you@example.com>
2020-07-29 18:07:54 +0900

    Test

---
+++ etcP.py
@@ -0,0 +1,10 @@
+import json
+
+def read():
+    text = open("/etc/passwd").read()
+    return text
+
+s1 = read().split("\n")
+# t = raw_input()
+for i in s1:
+    print json.dumps(i.split(":"), indent = 4)
